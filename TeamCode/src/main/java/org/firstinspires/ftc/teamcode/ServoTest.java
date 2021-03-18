eae0659d8f0c90ce955fc9c2e60110c875697c36
Erin Zhao <erin_zhao@horacemann.org>
2021-03-18 10:22:34 -0400

    made cr servo test
    - tests if wobble can continuously hold power
    - can hold a position set in start() !

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode;
+
+//trying to test stuff without the wobble goal
+
+
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import java.util.ArrayList;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/*
+7890 Space Lions 2019 "auton target red"
+author: 7890 Software
+GOALS: move to the target zone, park
+DESCRIPTION: This code moves us to the target zone using only a color sensor and then parks. We don't use the wobble goal mech in this code
+ */
+@Autonomous(name="servo test", group="Iterative Opmode")
+public class ServoTest extends OpMode {
+    CRServo wobble;
+
+    @Override
+    public void init() {
+
+        wobble = hardwareMap.crservo.get("wobble");
+
+    }
+
+    @Override
+    public void start() {
+        wobble.setPower(1.0);
+
+    }
+
+    public void loop() {
+
+        telemetry.addLine("we in loop bois");
+        telemetry.update();
+
+    }
+
+}
