5c80019c71249e0125492cd20ff92612bf4df45a
avi-rao <57546003+avi-rao@users.noreply.github.com>
2021-03-18 15:51:09 -0400

    Wrote WobbleMotorTest Code
    Wrote a test for wobble motor, it can spin the servo! The servo is also now mounted. Not janky I swear

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WobbleMotorTest.java
@@ -0,0 +1,75 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import java.util.ArrayList;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+import java.util.ArrayList;
+import java.util.List;
+@Autonomous(name="Wobble Motor Test", group="Iterative Opmode")
+public class WobbleMotorTest extends OpMode {
+    ArrayList<DcMotor> motors = new ArrayList<DcMotor>();
+
+    DcMotor leftFront;
+    DcMotor rightFront;
+    DcMotor leftBack;
+    DcMotor rightBack;
+    DcMotor center;
+    DcMotor wobblemotor;
+    CRServo wobble;
+
+    @Override
+    public void init() {
+        rightFront = hardwareMap.dcMotor.get("right front");
+        leftFront = hardwareMap.dcMotor.get("left front");
+        rightBack = hardwareMap.dcMotor.get("right back");
+        leftBack = hardwareMap.dcMotor.get("left back");
+        center = hardwareMap.dcMotor.get("center");
+        wobblemotor = hardwareMap.dcMotor.get("wobble motor");
+
+        //distSensor = hardwareMap.get(ModernRoboticsI2cRangeSensor.class, "distance sensor");
+
+        /*
+        ---MOTOR DIRECTIONS---
+         */
+        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        leftBack.setDirection(DcMotor.Direction.REVERSE);
+
+        /*
+        ---GROUPING---
+         */
+        motors.add(rightFront);
+        motors.add(leftFront);
+        motors.add(rightBack);
+        motors.add(leftBack);
+        motors.add(center);
+
+
+    }
+
+    @Override
+    public void loop() {
+        wobblemotor.setPower(.5);
+
+    }
+
+    public void wait(int time) {
+        try {
+            Thread.sleep(time * 1000);//milliseconds
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
